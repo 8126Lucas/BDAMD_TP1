cd505bc28c8f8de89df0fb4f3c495b2b03e769ef
lucas <lucaspereirasantos8126@gmail.com>
2025-10-25 19:56:49 +0100

    Conclusão do modelo_relacional.puml. Separação das responsabilidades para o model_creation e o model_destruction.

---
+++ src/model_creation_Lucas.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Item (
+
+);
+CREATE TABLE Item_Stock (
+
+);
+CREATE TABLE Warehouse (
+
+);
+CREATE TABLE Geographical_Zone (
+
+);
+CREATE TABLE Sell_Price (
+
+);
+CREATE TABLE Empolyee (
+
+);
+CREATE TABLE Client (
+
+);
